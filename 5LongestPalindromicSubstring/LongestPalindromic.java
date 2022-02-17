738c35366955c1af27eb98ed23daf185fbbe9116
Aglaia <aglaiabox@gmail.com>
2022-02-17 13:29:51 +0200

    Task LongestPalindromicSubstring little not important changing

--- 5LongestPalindromicSubstring/LongestPalindromic.java
+++ 5LongestPalindromicSubstring/LongestPalindromic.java
@@ -8,26 +8,30 @@
 import java.util.Map;
 
 public class LongestPalindromic {
+    static int o;
 
     public static void main(String[] args) {
-        String s = "c";
+        String s = "aaaaaaaaaa";
         System.out.println(longestPalindrome(s));
+        System.out.println(o);
     }
 
     public static String longestPalindrome(String s) {
-        String maxPolindrom = "";
-        String polindrom = "";
-        int begin;
-        int end;
-        boolean toCont = true;
+        String maxPolindrom = "", polindrom = "";
+        int begin, end;
+        boolean toCont;
 
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < 2; j++) {
                 begin = i; end = i+j;
+
+                if (end<s.length()) o=o+2;  // счетчик
+
                 if (end<s.length() && s.charAt(begin) == s.charAt(end) ){
                     toCont = true;
 
                     while (toCont){
+                        if (begin>0 && end<(s.length()-1)) o=o+2;      // счетчик
                         if (begin>0 && end<(s.length()-1) && s.charAt(begin-1) == s.charAt(end+1)){
                             --begin;
                             ++end;
