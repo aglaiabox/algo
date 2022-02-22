f6334da8e1071a833da499c5c75e54fd581712b5
Aglaia <aglaiabox@gmail.com>
2022-02-22 12:56:43 +0200

    +one simple solution (not mine) with LinkedHashMap Roman Number to Integer (medium)

---
+++ 13RomanToInteger/IntegerToRoman3.java
@@ -0,0 +1,35 @@
+import java.util.LinkedHashMap;
+
+public class IntegerToRoman3 {
+
+    public String intToRoman(int num) {
+        String res = "";
+        LinkedHashMap<Integer,String> map = new LinkedHashMap<>();
+
+        map.put(1000,"M");
+        map.put(900,"CM");
+        map.put(500,"D");
+        map.put(400,"CD");
+        map.put(100,"C");
+        map.put(90,"XC");
+        map.put(50,"L");
+        map.put(40,"XL");
+        map.put(10,"X");
+        map.put(9,"IX");
+        map.put(5,"V");
+        map.put(4,"IV");
+        map.put(1,"I");
+
+        while(num > 0){
+            for(int i : map.keySet()){
+                if(num >= i){
+                    res = res + map.get(i);
+                    num = num - i;
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
